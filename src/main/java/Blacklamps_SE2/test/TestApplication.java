6f330101d99cf4e9f6219ead0c5da71474a1a127
SNOOKIE-LAP\Snookie <pawansandaru@gmail.com>
2018-05-25 12:30:49 +0530

    Mevan Spring App test

---
+++ src/main/java/Blacklamps_SE2/test/TestApplication.java
@@ -0,0 +1,12 @@
+package Blacklamps_SE2.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
